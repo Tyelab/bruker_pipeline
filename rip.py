8945e7062289a96a7e26209edb0bc043ca568333
Jeremy Delahanty <jdelahanty@salk.edu>
2021-11-22 15:46:03 -0800

    Update rip.py

--- rip.py
+++ rip.py
@@ -34,11 +34,6 @@ def determine_ripper(data_dir, ripper_dir):
     root = tree.getroot()
     version = root.attrib['version']
 
-    # Prairie View versions are given in the form A.B.C.D.
-    match = re.match(r'\d+\.\d+\.\d+\.\d+$', version)
-    if not match:
-        raise RippingError('Could not parse version (expected A.B.C.D): %s' % version)
-    version = match.group(0)
     ripper = ripper_dir / f'{version}' / 'Utilities' / 'Image-Block Ripping Utility.exe'
     logger.info('Data created with Prairie version %s, using ripper: %s', version, ripper)
     return ripper
