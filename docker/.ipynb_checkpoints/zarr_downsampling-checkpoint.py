7fd93c73e2a2901d83110f1140a4fed0e288ebed
Jeremy Delahanty <jdelahanty@salk.edu>
2022-04-08 13:49:13 -0700

    Add new functions and data for subtraction
    Added in some basic visualizations for background subtraction testing using matplotlib

---
+++ docker/.ipynb_checkpoints/zarr_downsampling-checkpoint.py
@@ -0,0 +1,27 @@
+
+import zarr
+from skimage import exposure, img_as_ubyte
+import dask.array
+
+def downsample_zarr(dataset_path):
+    
+    original = zarr.open_array(dataset_path)
+
+    da = dask.array.from_zarr(original)
+
+    print(type(da))
+    
+    print("Loaded into zarr!")
+    
+    transformed = dask.array.map_blocks(lambda x=da: img_as_ubyte(exposure.rescale_intensity(x)), dtype='uint16')
+    
+    print("Rescaling complete!")
+    
+    zarr.save_array("snlkt/data/specialk/data2/2p_uint8.zarr", transformed)
+    
+    print("Zarr saved!")
+
+if __name__ == "main":
+    dataset_path = "/snlkt/data/specialk/data/2p.zarr/2p/"
+    
+    downsample_zarr(dataset_path)
